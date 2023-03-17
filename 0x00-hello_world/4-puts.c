85e39b0e484b51e7b4424ca5b288171425ac2e7d
olola <me@gmail.com>
2023-03-16 21:39:17 -0700

    After return5andsuccess

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio. h>
+#include <stdio.h>
 /**
  * main - firstc peogramming
  * Return: 0 (Success)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <sdio.h>
 /**
  * main - Online soln
- * Return: 0 (correct)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+return (0);
 }
