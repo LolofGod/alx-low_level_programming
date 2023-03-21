15def8eb816695f25403f7dad6c968ebb10e7eb2
olola <me@gmail.com>
2023-03-20 22:00:14 -0700

    nithing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - eje
+ *
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	char i;
+	;
+	for (i = 'a' : i <= 'z' : i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
